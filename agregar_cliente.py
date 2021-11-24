9f38642f9aa18003168404e1caccc77a65d06273
Brayan Riascos <briascosarroyo@gmail.com>
2021-11-23 21:58:19 -0500

    crear cliente

--- agregar_cliente.py
+++ agregar_cliente.py
@@ -8,21 +8,21 @@ class Cliente:
     #comenzamos con el metodo constructor
 
 
-    def __init__(self,nombre,numero_telefono,codigo_cuenta):
+    def __init__(self,nombre,numero_telefono, cuenta):
 
         self.nombre=nombre
         self.numero_telefono=numero_telefono
-        self.codigo_cuenta=codigo_cuenta
+        self.cuenta=cuenta
         
 
-    def modificar(self,nombre,numero_telefono,codigo_cuenta):
+    def modificar(self,nombre,numero_telefono,cuenta):
         self.nombre=nombre
         self.numero_telefono=numero_telefono
-        self.codigo_cuenta=codigo_cuenta
+        self.cuenta=cuenta
 
     #con la funcion de pasar a cadena lo que haremos es mostrar los atributos de los anteriormente creados
     #en una cadena de caracteres
 
     def PasarAcadena(self):
         print("")
-        return print("--Nombre--",self.nombre,"--Número de teléfono--",self.numero_telefono,"--ódigo de cuenta--",self.codigo_cuenta)
+        return print("--Nombre--",self.nombre,"--Número de teléfono--",self.numero_telefono,"--valor de la deuda--",self.cuenta)
--- main.py
+++ main.py
@@ -1,3 +1,48 @@
 #importamos la clase cliente para poder crear los objetos cliente
 from agregar_cliente import Cliente 
+lista_Cliente=[]
 
+
+def crear_cliente():
+        print("""
+         ------------------------------
+        ******Menú crear cliente******
+        ------------------------------""")
+        print("*")
+        nombre=input("Ingrese el nombre del cliente: ")
+        print("*")
+        telefono=input("Ingrese el numero telefonico del cliente: ")
+        print("*")
+        print("1-Abono ** 2-Deuda ** 3-inicia en (0)")
+        print("*")
+        tipo_cuenta=int(input("Para crear la cuenta debe confirmar si inicia con un abono, de una o en cero: "))
+        print("*")
+        if(tipo_cuenta==1):
+            cuenta_f=float(input("Ingrese el valor del abono en el que inicia la cuenta: "))
+            print("*")
+            cuenta=cuenta_f*(-1)
+        elif(tipo_cuenta==2):
+            cuenta=float(input("Ingrese el valor del la deuda en la que inicia la cuenta: "))
+            print("*")
+        elif(tipo_cuenta==3):
+            cuenta=0.0
+        else:
+            print("Datos invalidos")
+            print("*")
+        cliente_t=Cliente(nombre,telefono,cuenta)
+        lista_Cliente.append(cliente_t)
+        s=input("Desea segir creando clientes si/no : ")
+        if(s=="si"):
+            crear_cliente()
+        else:
+            print("fin")
+
+
+
+
+
+        
+
+                    
+
+  
